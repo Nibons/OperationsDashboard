b98876d865d4c9915ee9e892650b12d39aaa8855
Clay Haertzen <chaertzen@deloitte.com>
2017-09-28 09:41:37 -0400

    script will now check/remove/cleanup existing redis instances

--- Start-TestRedisInstance.ps1
+++ Start-TestRedisInstance.ps1
@@ -1,10 +1,27 @@
 #this starts a new redis instance, which we can use to test.
-docker run -d --name redis -p 6379:6379 redis:nanoserver 
-$redis_inspection = docker inspect redis | ConvertFrom-Json
-$list_network = $redis_inspection.NetworkSettings.Networks
-[string[]]$network_Names = $list_network | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty name
-foreach($netName in $network_Names){
-	$ipAddress = $list_network.$netName | Select-Object -ExpandProperty IPAddress
-	Write-Output "IP: $ipAddress"
+begin{
+	$redisInstance = docker ps -aq --filter name=redis
+	if($redisInstance){
+		docker rm -f $redisInstance
+	}
 }
-docker exec redis redis-cli monitor
+Process{
+	#start the instance
+	docker run -d --name redis -p 6379:6379 redis:nanoserver 
+
+	#retrieve data from the instance
+	$redis_inspection = docker inspect redis | ConvertFrom-Json
+	$list_network = $redis_inspection.NetworkSettings.Networks
+	[string[]]$network_Names = $list_network | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty name
+	foreach($netName in $network_Names){
+		$ipAddress = $list_network.$netName | Select-Object -ExpandProperty IPAddress
+		Write-Output "IP: $ipAddress"
+	}
+
+	#begin monitoring the instance, outputing to STDOUT
+	docker exec redis redis-cli monitor
+}
+End{
+	#stop+remove the instance
+	docker rm -f redis
+}
