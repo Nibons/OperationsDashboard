46d67ff4b4398c25c01385708a22203af36d6408
Clay Haertzen <chaertzen@deloitte.com>
2017-09-29 15:11:30 -0400

    moved RedisExample to common, removed ServiceStack.Redis dependency from other projects

--- Pinger/Startup.cs
+++ Pinger/Startup.cs
@@ -1,22 +1,10 @@
 ﻿using System;
-using ServiceStack.Redis;
+using OperationsDashboard.Common;
 
 namespace Pinger
 {
     class Startup
     {
-        protected string redisConnection { get; set; }        
-
-        public Startup()
-        {
-            this.setupRedis();
-        }
-
-        private void setupRedis()
-        {
-            var redisHostName = Environment.GetEnvironmentVariable("redis_hostname");
-            var redisPort = Environment.GetEnvironmentVariable("redis_port");
-            this.redisConnection = redisHostName + redisPort;
-        }
+        
     }
 }
