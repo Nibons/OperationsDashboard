eb169732ea064458aab541bd0b60545033c9d6ca
Clay Haertzen <chaertzen@deloitte.com>
2017-09-29 15:53:42 -0400

    updated property names to fit c# standars

--- HostConnection/ResponseData.cs
+++ HostConnection/ResponseData.cs
@@ -6,17 +6,17 @@ namespace OperationsDashboard.Common
 {
     public class ResponseData : EventArgs
     {
-        public string hostname { get; set; }
-        public string servername { get; set; }
-        public string friendlyname { get; set; }
-        public int httpResponse { get; set; }
+        public string Hostname { get; set; }
+        public string Servername { get; set; }
+        public string Friendlyname { get; set; }
+        public int HttpResponse { get; set; }
         public string IpAddress { get; set; }
-        public DateTime testTime { get; set; }
+        public DateTime TestTime { get; set; }
         public Uri Uri { get; set; }
 
         public ResponseData(int httpResponse, string IpAddress, Uri uri)
         {
-            this.testTime = DateTime.Now;
+            this.TestTime = DateTime.Now;
             this.Uri = uri;
             this.IpAddress = IpAddress;
         }
