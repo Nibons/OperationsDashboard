aef81a1e90b12c6e6139661e9ca59ad37137ad31
Clay Haertzen <chaertzen@deloitte.com>
2017-09-29 15:01:15 -0400

    added helper functions to deal with Environment Variables

---
+++ HostConnection/EnvVar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OperationsDashboard.Common
+{
+    class EnvVar
+    {
+        public static string AsString(string key)
+        {
+            return Environment.GetEnvironmentVariable(key);
+        }
+        public static int AsInt(string key)
+        {
+            return int.Parse(EnvVar.AsString(key));
+        }
+    }
+}
