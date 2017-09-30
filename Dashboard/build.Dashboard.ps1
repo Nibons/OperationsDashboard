dbd3cd499b411abcb617f387dc684d856b070e2b
Clay Haertzen <chaertzen@deloitte.com>
2017-09-30 15:42:05 -0400

    build commands now run correctly

--- Dashboard/build.Dashboard.ps1
+++ Dashboard/build.Dashboard.ps1
@@ -2,19 +2,19 @@ param([string]$configuration="Development",
 [string]$framework="netcoreapp2.0",
 [string]$outputPath="obj/$configuration/$framework/",
 [validateset("quiet","minimal","normal","detailed","diagnostic")]
-[string]$verbosity='normal'
+[string]$verbosity='minimal'
 )
-
-#compile the angular4App to wwwroot
-if($configuration -eq 'Production'){
-    #build as Production
-    ng build -prod --output-path=wwwroot
-}else{
-    ng build --output-path=wwwroot
-}
-
 #restore the dotNet dependencies
 dotnet restore
 
 #compile the dotNet depencies for running
 dotnet build --configuration $configuration --framework $framework --output $outputPath --verbosity $verbosity
+
+#compile the angular4App to wwwroot
+$wwwRootOutputPath = "$outputPath/wwwroot"
+if($configuration -eq 'Production'){
+    #build as Production
+    ng build -prod --output-path=$wwwRootOutputPath
+}else{
+    ng build --output-path=$wwwRootOutputPath
+}
