41a10b3a51d2aaefaf9b2a15b071840c415465b4
Clay Haertzen <chaertzen@deloitte.com>
2017-09-30 02:37:08 -0400

    successful test of retrieving IEnumerable<ServerMappingEntry> as JSON

---
+++ WebAPI/Controllers/ServerMappingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OperationsDashboard.Common;
+using ServiceStack.Redis;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ServerMapping")]
+    public class ServerMappingController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<ServerMappingEntry> Get()
+        {
+            using (var RedisConnection = new RedisClient(EnvVar.AsString("Redis_Hostname"), EnvVar.AsInt("Redis_Port")))
+            {
+                var RedisContext = RedisConnection.As<ServerMappingEntry>();
+                return RedisContext.GetAll();
+            }
+        }
+    }
+}
