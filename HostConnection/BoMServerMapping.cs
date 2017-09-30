fce1d2f62bfd503400c0f122dbfb5ac54c76926d
Clay Haertzen <chaertzen@deloitte.com>
2017-09-30 01:20:56 -0400

    Increased BoM load speed

--- HostConnection/BoMServerMapping.cs
+++ HostConnection/BoMServerMapping.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
 using CsvHelper;
 using System.Linq;
+using ServiceStack.Redis;
 
 namespace OperationsDashboard.Common
 {
     public class BoMServerMapping
     {
         private IEnumerable<ServerMappingEntry> ListServerMapping;
+        public string BoMLoadState { get; set; }
 
         void ReadBomAsCsv() => ReadBomAsCsv(EnvVar.AsString("BoM_CsvFileName"));
 
@@ -23,74 +25,30 @@ namespace OperationsDashboard.Common
 
         void AddAllToRedis()
         {
-            foreach(ServerMappingEntry ServerMappingEntry in ListServerMapping)
+            using (var RedisConnection = new RedisClient(EnvVar.AsString("Redis_Hostname"), EnvVar.AsInt("Redis_Port")))                
             {
-                ServerMappingEntry.AddToRedis();
-            }            
+                var ListServerMappingContext = RedisConnection.As<ServerMappingEntry>();
+                foreach (ServerMappingEntry ServerMappingEntry in ListServerMapping)
+                {
+                    ServerMappingEntry.id = ListServerMappingContext.GetNextSequence();
+
+                    //removed because it wasn't nearly as fast
+                    //string key = string.Format("servermapping:{0}:{1}:{2}", ServerMappingEntry.LogicalEnvironment, ServerMappingEntry.Servername, ServerMappingEntry.IPAddress);
+                    //ListServerMappingContext.SetValue(key, ServerMappingEntry);
+                    //ListServerMappingContext.Store(ServerMappingEntry);
+                }
+                ListServerMappingContext.StoreAll(ListServerMapping);
+            }
+                         
         }
         public BoMServerMapping(string filename = "")
         {
-            if(filename == "")
+            if (filename == "")
             {
                 filename = EnvVar.AsString("BoM_CsvFileName");
             }
             this.ReadBomAsCsv(filename);
-            this.AddAllToRedis();
-        }
-        //public string FilePath { get; set; }
-        //public IList<ServerMappingEntry> GetServerMappingEntryList() => GetServerMappingEntryList(EnvVar.AsString("BoM_URL"));
-        //public IList<ServerMappingEntry> GetServerMappingEntryList(string url)
-        //{
-
-        //}
-        //SecureString SetPassword()
-        //{
-        //    string query = string.Format("Please enter the password for {0}: ", EnvVar.AsString("BoM_DLUsername"));
-        //    Console.Write(query);
-        //    var pwd = new SecureString();
-        //    while (true)
-        //    {
-        //        ConsoleKeyInfo i = Console.ReadKey(true);
-        //        if (i.Key == ConsoleKey.Enter)
-        //        {
-        //            break;
-        //        }
-        //        else if (i.Key == ConsoleKey.Backspace)
-        //        {
-        //            if (pwd.Length > 0)
-        //            {
-        //                pwd.RemoveAt(pwd.Length - 1);
-        //                Console.Write("\b \b");
-        //            }
-        //        }
-        //        else
-        //        {
-        //            pwd.AppendChar(i.KeyChar);
-        //            Console.Write("*");
-        //        }
-        //    }
-        //    return pwd;
-        //}
-        //void DownloadBoM() => DownloadBoM(SetPassword());
-        //void DownloadBoM(SecureString password) => DownloadBoM(new PSCredential(EnvVar.AsString("BoM_DLUsername"), password));
-        //void DownloadBoM(PSCredential pSCredential) => DownloadBoM(pSCredential, EnvVar.AsString("BoM_URL"));
-        //void DownloadBoM(PSCredential pSCredential,string uri)
-        //{
-        //    PowerShell ps = PowerShell.Create()
-        //        .AddCommand("invoke-webRequest")
-        //        .AddParameter("Uri", uri)
-        //        .AddParameter("credential", pSCredential)
-        //        .AddParameter("UseBasicParsing", SwitchParameter.Present)
-        //        .AddParameter("OutFile", "$env:Temp/BoM.xlsx" );
-        //    ps.Invoke();
-        //}
-
-        //void ReadBom(string filePath) => ReadBom(filePath, EnvVar.AsString("BoM_WorksheetName"));        
-        //void ReadBom(string filePath,string worksheetName,string csvFileName="./Bom.csv")
-        //{
-        //    var excel = new Application();
-        //    var workbook = excel.Workbooks.Open(filePath);
-        //    workbook.Worksheets.Item(2);
-        //}
-    }
+            this.AddAllToRedis();            
+        }        
+    }    
 }
