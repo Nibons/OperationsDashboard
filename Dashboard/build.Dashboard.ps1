23dc63620db185ed9dd1946a5ce3d1374cb7856f
Clay Haertzen <chaertzen@deloitte.com>
2017-09-30 15:20:49 -0400

    added Dashboard to solution

---
+++ Dashboard/build.Dashboard.ps1
@@ -0,0 +1,20 @@
+param([string]$configuration="Development",
+[string]$framework="netcoreapp2.0",
+[string]$outputPath="obj/$configuration/$framework/",
+[validateset("quiet","minimal","normal","detailed","diagnostic")]
+[string]$verbosity
+)
+
+#compile the angular4App to wwwroot
+if($configuration -eq 'Production'){
+    #build as Production
+    ng build -prod --output-path=wwwroot
+}else{
+    ng build --output-path=wwwroot
+}
+
+#restore the dotNet dependencies
+dotnet restore
+
+#compile the dotNet depencies for running
+dotnet build --configuration $configuration --framework $framework --output $outputPath --verbosity $verbosity
