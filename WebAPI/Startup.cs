09c62c08e4256f55bd0b73654f73a33aba2a74ce
Clay Haertzen <chaertzen@deloitte.com>
2017-09-30 22:05:18 -0400

    potentially fixed issue with cross-domain origin

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -24,6 +24,7 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +34,10 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(builder =>
+                    builder.WithOrigins("*")
+                    .AllowAnyHeader()
+                );
 
             app.UseMvc();
         }
